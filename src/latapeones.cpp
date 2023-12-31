f7d3b129f9c4b7237878844f42e7769200b396ea
Jaime <jamartbr@gmail.com>
2023-12-31 18:57:49 +0100

    latapeones

--- src/latapeones.cpp
+++ src/latapeones.cpp
@@ -24,14 +24,25 @@ LataPeones::LataPeones() {
     ponerNombre("LataPeones");
 
     NodoGrafoEscena *lata = new NodoGrafoEscena();
-    lata->agregar(new Material(0.5, 0.2, 1.0, 20));
     lata->agregar(new MallaRevolPLY("lata-pinf.ply", 50));
+    lata->agregar(new Material(0.4, 0.5, 0.8, 50));
     lata->agregar(new MallaRevolPLY("lata-psup.ply", 50));
-    lata->agregar(new Material(new Textura("lata-coke.jpg"), 0.5, 0.4, 1.0, 10));
+    lata->agregar(new Material(new Textura("lata-coke.jpg"), 0.4, 1.0, 2.0, 50));
     lata->agregar(new MallaRevolPLY("lata-pcue.ply", 50));
-    
-    Objeto3D *peon = new MallaRevolPLY("peon.ply", 50);
-
-    // agregar(scale(vec3(0.5, 0.5, 0.5)));
     agregar(lata);
+    
+    NodoGrafoEscena *peon = new NodoGrafoEscena();
+    peon->agregar(new MallaRevolPLY("peon.ply", 50));
+    agregar(scale(vec3(0.16, 0.16, 0.16)));
+    agregar(translate(vec3(2.5, 1.4, 5.0)));
+    // peón sin textura, con un material puramente difuso (sin brillos especulares), de color blanco
+    agregar(new Material(1.0, 1.0, 0.0, 1));
+    agregar(peon);
+    // peón negro, sin textura, con un material especular sin apenas reflectividad difusa
+    agregar(new Material(0.0, 0.1, 2.0, 10));
+    agregar(translate(vec3(2.5, 0.0, 0.0)));
+    agregar(peon);
+    agregar(translate(vec3(-5.0, 0.0, 0.0)));
+    agregar(new Material(new Textura("text-madera.jpg"), 0.25, 1.5, 1.5, 100));
+    agregar(peon);
 }
